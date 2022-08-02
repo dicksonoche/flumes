42bb350ee8431f1d71f46367d59aa06c90c3b774
simon <simonoche987@gmail.com>
2022-08-02 07:42:15 +0100

    First commit

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Grid } from '@material-ui/core'
+
+import Product from './Product/Product'
+import useStyles from './styles'
+
+const products = [
+    { id: 1, name: 'Shoes', description: 'Running shoes.', price: '$5', image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaIDkktu1Szi_uYcrRh_at5uvZE0tx3usruQ&usqp=CAU" },
+    { id: 2, name: 'Bag', description: 'DG Bags.', price: '$15', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScN0Y8nk6tWeb7W_VYWdPXJld3mER0x1QypQ&usqp=CAU' },
+];
+
+const Products = () => {
+    const classes = useStyles();
+    return (
+        <main className={classes.content}>
+            <div className={classes.toolbar} />
+            <Grid container justify="center" spacing={4}>
+                {products.map((product) => (
+                    <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+                        <Product product={product} />
+                    </Grid>    
+                ))}
+            </Grid>
+        </main>
+    )
+}
+
+export default Products
